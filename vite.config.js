a729cade7165dc3606851b5e8ba3599c805d4dec
chunxiao01 <12861187+chunxiao01@users.noreply.github.com>
2024-03-09 09:46:28 +0800

    fix the bug.

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       manifest: {
         name: "新年快乐2023",
+        short_name: "新年快乐2023",
         description: "新年快乐，兔年大吉🐇！祝你天天开心，天天可爱！",
         theme_color: "#232323",
         icons: [
